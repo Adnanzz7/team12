2dd6705461f9ca5c0d637d98e12546f87e005e03
Ramzzz2888 <ammar.anf28@gmail.com>
2025-04-11 11:29:04 +0700

    Register & Web

--- Pendaftaran-LKS/routes/web.php
+++ Pendaftaran-LKS/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentAuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/register', [StudentAuthController::class, 'showRegisterForm'])->name('register.form');
+
+
+Route::post('/register', [StudentAuthController::class, 'register'])->name('register.student');
+
+
+Route::get('/login', [StudentAuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [StudentAuthController::class, 'login'])->name('login.process');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
